68a8f39f2cf96c145d50c9e79d8394e3ca167ef0
Filip Klimeš <filip@filipklimes.cz>
2022-01-12 21:43:14 +0100

    day 22

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 extern crate blockingqueue;
 extern crate itertools;
 extern crate priority_queue;
+extern crate regex;
 
-use crate::day23::day23;
+use crate::day22::day22;
 
-mod day23;
+mod day22;
 mod toolbox;
 
 fn main() {
-    day23();
+    day22();
 }
